862b39db9e8f73c39501a21e63a8a76b5fe5588e
DevIncomin <bdr123654@gmail.com>
2023-03-08 17:56:21 +0300

    Update main.py
    Replaced press_and_release for press then sleep then release, because press_and_release of keyboard module isn't considered a thing for the game.k

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pynput.mouse import Listener
 from pynput.mouse import Button
-
-import keyboard
+from keyboard import press, release
+from time import sleep # uses sleep because press_and_release isn't considered an output for the game
 
 # # Make sure to edit the key configurations in the game to matchup with the keys below, or edit them to your will.
 # LMB
@@ -13,14 +13,19 @@ skill_change_down = "f"
 
 def on_click(x, y, button, pressed):
     if button == Button.left:
-        if pressed: keyboard.press(attack)
-        else: keyboard.release(attack)
+        if pressed: press(attack)
+        else: release(attack)
 
 def on_scroll(x, y, dx, dy):
     if dy > 0:
-        keyboard.press_and_release(skill_change_up)
+        press(skill_change_up)
+        sleep(0.25)
+        release(skill_change_up)
+        
     if dy < 0:
-        keyboard.press_and_release(skill_change_down)
+        press(skill_change_down)
+        sleep(0.25)
+        release(skill_change_down)
         
 
 with Listener(on_scroll=on_scroll, on_click=on_click) as listener:
